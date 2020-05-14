ba8b13ad4ece9e5e205e7f12988bdb34f2ea08be
Edison Garcia Chinas <edisga@microsoft.com>
2020-05-14 09:40:29 -0500

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaultyApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        ArrayList objectList = new ArrayList();
+        private Array SaveObjects(int objectId)
+        {
+            int numbers = objectId / 8;
+            int[]arr = new int[numbers];
+
+            for (int i = 0; i < numbers; i++)
+            {
+                arr[i] = i;
+            }
+
+            return arr;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            var id = 400000 * 1024;
+            var objectId = SaveObjects(id);
+            objectList.AddRange(objectId);
+            return "Command Executed";
+        }
+    }
+}
